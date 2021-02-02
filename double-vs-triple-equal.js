81e3964080e9d7ef81f65c5c80bd3e3ee7bcb393
JisanFahim <JisanFahim@users.noreply.github.com>
2021-02-02 23:24:00 +0600

    double-vs-triple

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,40 @@
+const one = 5;
+const two = '5';
+
+// double equal
+if (one == two) {
+    console.log('For double equal: Condition is true');
+}
+else {
+    console.log('For double equal: Condition is false')
+}
+
+
+// triple equal
+if (one === two) {
+    console.log('For triple equal: Condition is true');
+}
+else {
+    console.log('For triple equal: Condition is false')
+}
+
+
+let first = 1;
+let second = true;
+
+if (first == second) {
+    console.log('Condition is true');
+}
+else {
+    console.log('Condition is false');
+}
+
+
+let three = 0;
+let four = false;
+if (three === four) {
+    console.log('Condition is true');
+}
+else {
+    console.log('Condition is false');
+}
